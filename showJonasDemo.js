ac839567f6612029d8a88f0687a65ebccc6bdc5a
wulfwulf <jonas.wulfandersen@gmail.com>
2020-02-05 20:17:03 +0100

    Update showJonasDemo.js
    Jonas har løst den første opgave.

--- showJonasDemo.js
+++ showJonasDemo.js
@@ -1,7 +1,7 @@
 
 function showJonasDemo() {
     var page = getPageFromURL();
-    var boxsize = 20 ;
+    var boxsize = 20;
     var c = document.getElementById("jonasCanvas");
     var ctx = c.getContext("2d");
 
@@ -11,13 +11,28 @@ function showJonasDemo() {
             for (i = 0; i < 10; i++) {
                 var x = Math.floor(Math.random() * (c.height-boxsize));
                 var y = Math.floor(Math.random() * (c.width-boxsize));
-                ctx.rect(x, y, boxsize, boxsize);
+                ctx.rect(x, y, boxsize, boxsize);           
             }
+        
+        break;
+        case 2:
+            var xArray = new Array();
+            var yArray = new Array();
+            for (i = 0; i < 10; i++) {
+              xArray.push(Math.floor(Math.random() * (c.height-boxsize)));
+              yArray.push(Math.floor(Math.random() * (c.height-boxsize)));
+            }
+            for (i = 0; i < xArray.length; i++) {
+                if (xArray[i] < 200){
+                    ctx.rect(xArray[i], yArray[i], boxsize, boxsize);
+                }
+            }
+
         break;
         default:
             ctx.rect(10, 10, boxsize, boxsize);
     }
-    ctx.rect(10, 10, boxsize, boxsize);
+    //ctx.rect(10, 10, boxsize, boxsize);
     ctx.stroke();
     var c = document.getElementById("programmedbyID3");
     c.innerHTML = "Programmed by Jonas"
