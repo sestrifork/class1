68793490c4c09ecfb5938085acfa7377ea0991f5
tobi7211 <tobi7211@gmail.com>
2020-02-22 16:19:21 +0100

    Update showTobiasDemo.js

--- showTobiasDemo.js
+++ showTobiasDemo.js
@@ -1,7 +1,7 @@
 
 function showTobiasDemo() {
     var page = getPageFromURL();
-    var boxsize = 20 ;
+    var boxsize = 50 ;
     var c = document.getElementById("tobiasCanvas");
     var ctx = c.getContext("2d");
 
@@ -14,10 +14,28 @@ function showTobiasDemo() {
                 ctx.rect(x, y, boxsize, boxsize);
             }
         break;
+        case 3:
+            ctx.rect(100, 100, boxsize, boxsize);
+            var rektangler = 10;
+            for (i = 0; i < rektangler; i++) {
+                var x = Math.floor(Math.random() * (c.height-boxsize));
+                var y = Math.floor(Math.random() * (c.width-boxsize));
+                
+
+                if(x>100 && x<100+boxsize && y> 100 && y<100+boxsize) {
+                    ctx.strokeStyle = "#FF0000";
+                }
+                
+                else { 
+                    ctx.strokeStyle = "#000000";
+                }
+                ctx.rect(x, y, boxsize, boxsize);
+            }
+        
+        break;
         default:
             ctx.rect(10, 10, boxsize, boxsize);
     }
-    ctx.rect(10, 10, boxsize, boxsize);
     ctx.stroke();
     var c = document.getElementById("programmedbyID2");
     c.innerHTML = "Programmed by Tobias"
